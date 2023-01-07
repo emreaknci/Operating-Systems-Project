aebde4eb4c5c4c258f87fd4feee52d611932577e
Beytullah Yayla <beytullah.yayla@ogr.sakarya.edu.tr>
2023-01-07 14:30:15 +0300

    Main Class Updated

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Dosya dosya = new Dosya();
+        List<Process> list = dosya.oku("giris.txt");
+
+        Dispatcher dispatcher = new Dispatcher(list);
+        dispatcher.run();
 	
 	}
 
