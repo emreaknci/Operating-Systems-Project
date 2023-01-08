3799e067f5f8858bcaf3a94af0425f3482252652
Yunus Emre Akıncı <emreakinci696@gmail.com>
2023-01-08 22:32:27 +0300

    Jar file added

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Dosya dosya = new Dosya();
-        List<Process> list = dosya.oku("giris.txt");//Proses listesini okuyoruz
+        List<Process> list = dosya.oku(args[0]);//Proses listesini okuyoruz
 
         Dispatcher dispatcher = new Dispatcher(list);//Dispatcher ile sırasıyla prosesleri çalıştırıyoruz
         dispatcher.run();
-	
+        
 	}
 
 }
